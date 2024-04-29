8f1fa3ef0c0af649d7d2b8bbddca2e2c0c926fbe
ratsam93 <ratsam93@gmail.com>
2024-04-29 21:52:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def read_html_file(html_file_path):
 def send_emails_from_csv(csv_file, html_file):
     # Read HTML content from file
     html_content = read_html_file(html_file)
-    count=0
+    # count=0
     with open(csv_file, newline='') as file:
         reader = csv.reader(file)
         for row in reader:
             # name = row[1]  # Assuming name is in the second column
             email = row[0]  # Assuming email is in the first column
-            count+=1
+            # count+=1
             email_params = {
                 "from": "Shreyas@email.maneuverventures.com",
                 "to": email,
@@ -35,8 +35,8 @@ def send_emails_from_csv(csv_file, html_file):
             }
             email = resend.Emails.send(email_params)
             # print(email)
-            if(count%100==0):
-                print(count)
+            # if(count%100==0):
+            #     print(count)
 
 
 # Call the function and pass the path to your CSV file and HTML file
